64de6e129e59cd545499a1abd7a59565c79d998d
pinlunCH <61253232+pinlunCH@users.noreply.github.com>
2022-01-06 17:31:56 -0800

    second commit: add README, html, and js files

---
+++ NavMenu.js
@@ -0,0 +1,26 @@
+NavMenu = (show, div, close) => {
+    const showBtn = document.querySelector('.' + show);
+        navDiv = document.querySelector('.' + div);
+        closeBtn = document.querySelector('.' + close);
+
+    showBtn.addEventListener('click', () => {
+        showBtn.classList.add('active');
+        navDiv.style.right = 'put in num';
+        navDiv.classList.add('active');
+        closeBtn.classList.add('active');
+        showBtn.classList.add('off');
+    });
+    // when showBtn is clicked, hide the showBtn and show the closeBtn
+    // Display navDiv
+
+    closeBtn.addEventListener('click', () => {
+        showBtn.classList.remove('active');
+        navDiv.style.right = 'put in num';
+        navDiv.classList.remove('active');
+        showBtn.classList.remove('off');
+        closeBtn.classList.remove('active');
+    });
+    // when closeBtn is clicked, hide the closeBtn and show the showBtn
+    // hide the navDiv
+}
+NavMenu('nav-toggle', 'standard-nav', 'nav-close');
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Reusable_nav
+# Reusable code
+## Show & Hide Navigation
+
+Need to be modify:
+* nav-toggle(Class Name)
+* standard-nav(Class Name)
+* nav-close(Class Name)
+* put in num(style.right number)
+
+CSS
+```
+{
+.nav-toggle.off{
+    display: none;
+}
+.nav-toggle{}
+.nav-close{
+    display: none;
+}
+.nav-close.active{
+    display: block;
+}
+```
